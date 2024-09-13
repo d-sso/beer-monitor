a6de86e62aab1fc6f32a341b3edd2a3017d996ba
valves <vinicius@nitro5-debian.local>
2024-09-13 16:24:17 -0500

    Generalized URL in frontend app

--- frontend/src/app/drinks.service.ts
+++ frontend/src/app/drinks.service.ts
@@ -2,16 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Drinker } from './shared/drinker';
 import { Observable } from 'rxjs';
+import { PlatformLocation } from '@angular/common';
+import { TemplateBindingParseResult } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DrinksService {
 
-  constructor(private http: HttpClient) { }
+export class DrinksService {
+  
+  base_url:string = "";
+
+  constructor(private http: HttpClient, location: PlatformLocation) { 
+    this.base_url = location.protocol + "//" + location.hostname + ":8000";
+    console.log(location.protocol);
+    console.log(location.hostname);
+    console.log(this.base_url);
+  }
 
   getDrinkers(): Observable<Drinker[]>{
-    return this.http.get<Drinker[]>('http://ip:8000/users');
+    return this.http.get<Drinker[]>(this.base_url + '/users');
   }
 
   removeDrinker(person:Drinker){
@@ -19,10 +29,10 @@ export class DrinksService {
   }
 
   addDrinker(person:Drinker){
-    this.http.post('http://ip:8000/adduser',person).subscribe(x => console.log(x));
+    this.http.post(this.base_url+'/adduser',person).subscribe(x => console.log(x));
   }
 
   setActive(id:number): Observable<any>{
-    return this.http.put(`http://ip:8000/user/active/${id}`,null);
+    return this.http.put(`${this.base_url}/user/active/${id}`,null);
   }
 }
